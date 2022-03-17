fd85af1393b84f3035f74a9f05835d49a851498c
KirillZhdanov <kzhdanau@topcon.com>
2022-03-17 12:18:19 +0300

    feat(App): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Users from './pages/Users'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload test.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/users">Users</Link>
+        <Link to="/">Home</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="users" element={<Users />} />
+      </Routes>
     </div>
   )
 }
